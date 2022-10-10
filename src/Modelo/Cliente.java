0a14708b5a06000a3a3bb0f11c63b069fd53dfad
F_Valladares2 <felipe.valladares2201@alumnos.ubiobio.cl>
2022-10-10 19:12:20 -0300

    implementación básica de lo visto en el laboratorio, -clase Cliente completada según diagrama -Main rellenado con código del profesor ajustado según diagrama -varios "TODO:" añadidos

--- src/Modelo/Cliente.java
+++ src/Modelo/Cliente.java
@@ -1,4 +1,42 @@
 package Modelo;
 
 public class Cliente {
+    private String rut;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private boolean activo;
+
+    //constructor
+    public Cliente(String rut, String nom, String tel, String dir) {
+        this.rut = rut;
+        nombre = nom;
+        telefono = tel;
+        direccion = dir;
+        activo = true;
+    }
+
+    //getters autogenerados
+    public String getRut() {
+        return rut;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public boolean isActivo(){
+        return activo;
+    }
+    public void setActivo(){
+        activo = true;
+    }
+    public void setInactivo(){
+        activo = false;
+    }
 }
